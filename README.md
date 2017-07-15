c999a59948fc30f5d6d47e881b40a4bb00f9c331
ackman678 <james.ackman@gmail.com>
2017-07-14 20:38:41 -0700

    first ver

---
+++ README.md
@@ -0,0 +1,5 @@
+---
+author: James Ackman  
+title: markdown-batch readme  
+date: 2017-07-13 16:59:58  
+---
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-//Batch convert markdown files to html
-const srcPath = '_posts';
-const dstPath = '_site';
-const overwrite = true;
-const extFilter = ['*.md','*.txt','*.mmd','*.markdown'];
+// Batch convert markdown files to html
+// const mdbatch = require('markdown-batch')
+// mdbatch(srcPath, dstPath, overwrite, extFilter, options)
+// mdbatch('_posts', '_site', true)
+
+// const srcPath = '_posts';
+// const dstPath = '_site';
+// const overwrite = true;
+// const extFilter = ['*.md','*.txt','*.mmd','*.markdown'];
 
 const fs = require('fs');
 const path = require('path');
@@ -22,26 +26,26 @@ const md = require('markdown-it')({
 
 const readFile = fileName => new Promise((resolve, reject) => {
   fs.readFile(fileName, 'utf8', (err, data) => {
-    if (err) return reject(err);
-    resolve(data);
+    if (err) reject(err);
+    else resolve(data);
   });
 });
 
 const writeFile = (fileName, datain) => new Promise((resolve, reject) => {
   fs.writeFile(fileName, datain, 'utf8', (err, data) => {
-    if (err) return reject(err);
-    resolve(data);
+    if (err) reject(err);
+    else resolve(data);
   });
 });
 
 const readdir = srcPath => new Promise((resolve, reject) => {
     fs.readdir(srcPath, (err, files) => {
-        if (err) return reject(err);
-        resolve(files);
+        if (err) reject(err);
+        else resolve(files);
     });
 });
 
-const handleFile = data => {
+const handleFile = (data,file) => {
   try {
     // console.log(file);
     let content = fm(data);
@@ -50,19 +54,20 @@ const handleFile = data => {
     return content.body;
   } 
   catch (err) {
-    console.log(`Error: ${err.name} file: ${file}\n  ${err.reason}, continuing with whole file...\n`);
+    console.log(`Warning: ${err.name} file: ${file}\n  ${err.reason}, continuing with entire file...\n`);
     return data;
   };
 };
 
-const processFiles = () => {
+
+const processFiles = (srcPath, dstPath, extFilter, options) => {
   //async process chain begin
   readdir(srcPath)
-  .then(allfiles => { return mm(allfiles,extFilter) })
+  .then(allfiles => { return mm(allfiles,extFilter,options) })
   .then(files => {
     files.forEach(file => {
       readFile(path.join(srcPath,file))
-      .then(data => { return handleFile(data) })
+      .then(data => { return handleFile(data,file) })
       .then(text => { return md.render(text) })
       .then(html => {
           writeFile(path.join(dstPath,path.parse(file).name + '.html'), html);
@@ -73,14 +78,24 @@ const processFiles = () => {
   .catch (err =>{ console.log(err) });
 };
 
-//app start
-fs.mkdir(dstPath, (err, dstPath) => {
-    if (err && !overwrite) {
-      console.log(`Error: ${err.message}. No files processed since overwrite: ${overwrite}`);
-    } else if (err && overwrite) {
-      console.log(`Warning: ${err.message}. Overwriting files since overwrite: ${overwrite}`);
-      processFiles();
-    } else {
-      processFiles();
-    }
-});
+function markdownBatch(
+  srcPath='.', 
+  dstPath='_site', 
+  overwrite=false, 
+  extFilter=['*.md','*.txt','*.mmd','*.markdown'],
+  options={matchBase: false, ignore: ['.git', 'node_modules', 'README.md', 'test', 'lib', 'css']}
+  ) {
+  //app start
+  fs.mkdir(dstPath, (err, undefined) => {
+      if (err && !overwrite) {
+        console.log(`Error: ${err.message}. No files processed since overwrite: ${overwrite}`);
+      } else if (err && overwrite) {
+        console.log(`Warning: ${err.message}. Overwriting files since overwrite: ${overwrite}`);
+        processFiles(srcPath, dstPath, extFilter, options);
+      } else {
+        processFiles(srcPath, dstPath, extFilter, options);
+      }
+  });
+}
+
+module.exports = markdownBatch;
